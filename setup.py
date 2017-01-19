26df9cc139ccddb5a77d5f6a1a6b47a0b147c21d
Esben Sonne <esbensonne@gmail.com>
2017-01-19 09:22:06 +0100

    Bump status to beta

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     license='MIT',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.3',
