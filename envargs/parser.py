51e4f1243042296d430e718e53ff655adc2fb266
Esben Sonne <esbensonne@gmail.com>
2018-02-02 13:18:43 +0100

    Added the ability to specify nested structures

--- envargs/parser.py
+++ envargs/parser.py
@@ -2,27 +2,31 @@
 import os
 
 from . import errors
+from . import models
 
 
 def _load_values(values, fields):
     """Load values according to the fields."""
     for dest, field in fields.items():
-        location = field.load_from or dest
-        value = values.get(location)
+        if isinstance(field, models.Var):
+            location = field.load_from or dest
+            value = values.get(location)
 
-        if value is None:
-            if field.default is None:
-                raise errors.ParseError(
-                    'Required field "{}" missing.'.format(location),
-                    location=location,
-                )
+            if value is None:
+                if field.default is None:
+                    raise errors.ParseError(
+                        'Required field "{}" missing.'.format(location),
+                        location=location,
+                    )
 
-            yield dest, field.default
+                yield dest, field.default
 
-        else:
-            value = field.parse(value, location)
-            field.validate(value, location)
-            yield dest, value
+            else:
+                value = field.parse(value, location)
+                field.validate(value, location)
+                yield dest, value
+        elif isinstance(field, dict):
+            yield dest, dict(_load_values(values, field))
 
 
 def parse_dict(values, fields):
